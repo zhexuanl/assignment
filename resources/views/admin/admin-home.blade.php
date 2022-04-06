4d96fa51b58d86c09caace8c2de4e1e89787b4d1
Goh Sen Fai <senfai2000@hotmail.com>
2022-04-06 16:24:05 +0800

    Created auth and admin

---
+++ resources/views/admin/admin-home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.auth')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+                <div class="card-body">
+                    <button>Hi</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth:admin'], function () {
+
+    //Home page- view hall
+    Route::get('/admin/home', [AdminController::class, 'index']);
+
+    //Add a new hall
+    Route::view("/admin/addHall", "/admin/addHall");
+    Route::post("/admin/addHall", [HallController::class, 'addHall']);
+
+    //Edit an existing hall
+    Route::get('/admin/editHall/{id}', [HallController::class, 'showEdit']);
+    Route::post('/admin/editHall/{id}', [HallController::class, 'editHall']);
+
+    //Delete an existing hall
+
+
+
 });
