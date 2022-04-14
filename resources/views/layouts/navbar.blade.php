a700f5afb8e2b9d8440a11397243f728876cb08a
Foo <shidian02@gmail.com>
2022-04-14 11:50:48 +0800

    changes to nav bar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -45,10 +45,10 @@
             <li class="nav-item mt-md-2 mr-md-2">
                 <a class="nav-link-item nav-header" href="{{ '/admin/home'}}">{{ __('Home') }}</a>
             </li>
+            @elsecan('isUser')
             <li class="nav-item pr-5">
                 <a class="nav-link nav-header" href="bookings">Manage Bookings</a>
             </li>
-            @elsecan('isUser')
             <li class="nav-item mt-md-2 mr-md-2">
                 <a class="nav-link-item nav-header" href="{{ '/user/home'}}">{{ __('Home') }} |</a>
             </li>
